1f33b494d954628ab09870a0df6a6f7f1b7d7e1f
MEFRREEX <mefrreex@gmail.com>
2025-01-02 17:14:56 +0200

    docs(javadoc): added javadoc for script function exceptions

--- api/src/main/java/com/instancify/scriptify/api/exception/ScriptFunctionArgTypeException.java
+++ api/src/main/java/com/instancify/scriptify/api/exception/ScriptFunctionArgTypeException.java
@@ -1,7 +1,16 @@
 package com.instancify.scriptify.api.exception;
 
+/**
+ * Exception for script function argument type mismatches.
+ */
 public class ScriptFunctionArgTypeException extends ScriptFunctionException {
 
+    /**
+     * Creates an exception when argument types don't match.
+     *
+     * @param required The expected type
+     * @param provided The type that was given
+     */
     public ScriptFunctionArgTypeException(Class<?> required, Class<?> provided) {
         super("Required argument type: " + required + ", passed: " + provided);
     }
--- api/src/main/java/com/instancify/scriptify/api/exception/ScriptFunctionException.java
+++ api/src/main/java/com/instancify/scriptify/api/exception/ScriptFunctionException.java
@@ -1,15 +1,34 @@
 package com.instancify.scriptify.api.exception;
 
+/**
+ * Custom exception for errors in script functions.
+ */
 public class ScriptFunctionException extends Exception {
 
+    /**
+     * Creates a new ScriptFunctionException with the specified message.
+     *
+     * @param message the detail message
+     */
     public ScriptFunctionException(String message) {
         super(message);
     }
 
+    /**
+     * Creates a new ScriptFunctionException with the specified message and cause.
+     *
+     * @param message the detail message
+     * @param cause the cause of the exception
+     */
     public ScriptFunctionException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Creates a new ScriptFunctionException with the specified cause.
+     *
+     * @param cause the cause of the exception
+     */
     public ScriptFunctionException(Throwable cause) {
         super(cause);
     }

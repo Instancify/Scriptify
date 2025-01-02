6030fa2e2a14236dca6bae56bdea0cb3f70f19b8
MEFRREEX <mefrreex@gmail.com>
2025-01-02 18:26:16 +0200

    docs(javadoc): added javadoc for api classes

--- api/src/main/java/com/instancify/scriptify/api/script/Script.java
+++ api/src/main/java/com/instancify/scriptify/api/script/Script.java
@@ -2,9 +2,21 @@ package com.instancify.scriptify.api.script;
 
 import com.instancify.scriptify.api.script.function.ScriptFunctionManager;
 
+/**
+ * Defines the structure of a script that can be executed.
+ */
 public interface Script {
 
+    /**
+     * Sets the function manager for this script.
+     *
+     * @param functionManager The manager handling script functions
+     * @see ScriptFunctionManager
+     */
     void setFunctionManager(ScriptFunctionManager functionManager);
 
+    /**
+     * Evaluates and executes this script.
+     */
     void eval();
 }

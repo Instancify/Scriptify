a475f8fba5ab3f87d2448a039290332650f837ec
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2025-01-03 11:36:33 +0300

    feat: constants

--- core/src/main/java/com/instancify/scriptify/core/script/constant/StandardConstantManager.java
+++ core/src/main/java/com/instancify/scriptify/core/script/constant/StandardConstantManager.java
@@ -2,6 +2,8 @@ package com.instancify.scriptify.core.script.constant;
 
 import com.instancify.scriptify.api.script.constant.ScriptConstant;
 import com.instancify.scriptify.api.script.constant.ScriptConstantManager;
+import com.instancify.scriptify.core.script.constant.impl.ScriptConstantBaseDir;
+import com.instancify.scriptify.core.script.constant.impl.ScriptConstantOsName;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +11,11 @@ import java.util.Map;
 public class StandardConstantManager implements ScriptConstantManager {
 
     private final Map<String, ScriptConstant> constants = new HashMap<>();
+
+    public StandardConstantManager() {
+        register(new ScriptConstantOsName());
+        register(new ScriptConstantBaseDir());
+    }
 
     @Override
     public Map<String, ScriptConstant> getConstants() {

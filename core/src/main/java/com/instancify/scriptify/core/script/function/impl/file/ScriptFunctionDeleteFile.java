b209b656419a79370efe53bcd8b6ce2af66cec90
Koshak_Mine <52908036+KoshakMineDEV@users.noreply.github.com>
2025-01-03 12:00:08 +0300

    refactor: ScriptFunctionDeleteFile

--- core/src/main/java/com/instancify/scriptify/core/script/function/impl/file/ScriptFunctionDeleteFile.java
+++ core/src/main/java/com/instancify/scriptify/core/script/function/impl/file/ScriptFunctionDeleteFile.java
@@ -7,6 +7,7 @@ import com.instancify.scriptify.api.script.Script;
 import com.instancify.scriptify.api.script.function.ScriptFunction;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Represents a function to delete a file in the normal or recursive way
@@ -18,41 +19,37 @@ public class ScriptFunctionDeleteFile implements ScriptFunction {
         return "deleteFile";
     }
 
-    // TODO refactor it
     @Override
     public Object invoke(Script script, Object[] args) throws ScriptFunctionException {
-        if (args.length == 1) {
-            if (args[0] instanceof String filePath) {
-                try {
-                    return new File(filePath).delete();
-                } catch (Exception e) {
-                    return null;
-                }
-            } else {
-                throw new ScriptFunctionArgTypeException(String.class, args[0].getClass());
-            }
-        } else if (args.length == 2) {
-            if (args[0] instanceof String filePath) {
-                if (args[1] instanceof Boolean recursive) {
-                    try {
-                        File file = new File(filePath);
-                        if (recursive) {
-                            deleteDirectoryRecursively(file);
-                            return null;
-                        } else {
-                            return file.delete();
-                        }
-                    } catch (Exception e) {
-                        return null;
-                    }
-                } else {
-                    throw new ScriptFunctionArgTypeException(Boolean.class, args[1].getClass());
-                }
-            } else {
-                throw new ScriptFunctionArgTypeException(String.class, args[0].getClass());
-            }
-        } else {
+        if (args.length > 2 || args.length < 1) {
             throw new ScriptFunctionArgsCountException(1, args.length);
+        }
+        if (!(args[0] instanceof String filePath)) {
+            throw new ScriptFunctionArgTypeException(String.class, args[0].getClass());
+        }
+
+        if (args.length == 1) {
+            try {
+                return new File(filePath).delete();
+            } catch (Exception e) {
+                return null;
+            }
+        }
+
+        if (!(args[1] instanceof Boolean recursive)) {
+            throw new ScriptFunctionArgTypeException(Boolean.class, args[1].getClass());
+        }
+
+        try {
+            File file = new File(filePath);
+            if (recursive) {
+                deleteDirectoryRecursively(file);
+                return null;
+            } else {
+                return file.delete();
+            }
+        } catch (Exception e) {
+            return null;
         }
     }
 

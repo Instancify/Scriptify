5da5b6c5a00e5bbe52bb9dbf62ef8c6ea926e02c
MEFRREEX <mefrreex@gmail.com>
2025-03-29 19:35:59 +0400

    fix: added java type convert for graalvm js implementation

--- script-js-graalvm/src/main/java/com/instancify/scriptify/script/JsFunction.java
+++ script-js-graalvm/src/main/java/com/instancify/scriptify/script/JsFunction.java
@@ -4,8 +4,11 @@ import com.instancify.scriptify.api.exception.ScriptFunctionException;
 import com.instancify.scriptify.api.script.Script;
 import com.instancify.scriptify.api.script.function.ScriptFunction;
 import com.instancify.scriptify.api.script.function.argument.ScriptFunctionArgument;
-import org.graalvm.polyglot.proxy.ProxyExecutable;
 import org.graalvm.polyglot.Value;
+import org.graalvm.polyglot.proxy.ProxyExecutable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsFunction implements ProxyExecutable {
 
@@ -21,12 +24,37 @@ public class JsFunction implements ProxyExecutable {
     public Object execute(Value... arguments) {
         ScriptFunctionArgument[] args = new ScriptFunctionArgument[arguments.length];
         for (int i = 0; i < arguments.length; i++) {
-            args[i] = ScriptFunctionArgument.of(arguments[i].as(Object.class));
+            args[i] = ScriptFunctionArgument.of(convertValue(arguments[i]));
         }
         try {
             return function.invoke(script, args);
         } catch (ScriptFunctionException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Converts the Value object to the appropriate Java type.
+     */
+    private Object convertValue(Value value) {
+        if (value.isNull()) {
+            return null;
+        } else if (value.hasArrayElements()) {
+            List<Object> list = new ArrayList<>();
+            for (long j = 0; j < value.getArraySize(); j++) {
+                list.add(convertValue(value.getArrayElement(j)));
+            }
+            return list;
+        } else if (value.isHostObject()) {
+            return value.asHostObject();
+        } else if (value.isString()) {
+            return value.asString();
+        } else if (value.isBoolean()) {
+            return value.asBoolean();
+        } else if (value.isNumber()) {
+            return value.as(Number.class);
+        } else {
+            return value.as(Object.class);
         }
     }
 }

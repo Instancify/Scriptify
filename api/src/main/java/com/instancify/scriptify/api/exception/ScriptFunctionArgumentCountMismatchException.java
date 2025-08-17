5072e17810674c44bd5492475a99b5b465e03a7b
MEFRREEX <mefrreex@gmail.com>
2025-08-17 23:58:04 +0400

    chore: added javadoc for new exceptions

--- api/src/main/java/com/instancify/scriptify/api/exception/ScriptFunctionArgumentCountMismatchException.java
+++ api/src/main/java/com/instancify/scriptify/api/exception/ScriptFunctionArgumentCountMismatchException.java
@@ -1,11 +1,25 @@
 package com.instancify.scriptify.api.exception;
 
+/**
+ * Exception thrown when a function is called with
+ * an incorrect number of arguments.
+ */
 public class ScriptFunctionArgumentCountMismatchException extends ScriptFunctionException {
 
-    public ScriptFunctionArgumentCountMismatchException(String functionName,
-                                                        int expectedMin,
-                                                        int expectedMax,
-                                                        int provided) {
+    /**
+     * Creates a new exception for a mismatch in argument count.
+     *
+     * @param functionName The name of the function
+     * @param expectedMin The minimum number of expected arguments
+     * @param expectedMax The maximum number of expected arguments
+     * @param provided The number of arguments actually provided
+     */
+    public ScriptFunctionArgumentCountMismatchException(
+            String functionName,
+            int expectedMin,
+            int expectedMax,
+            int provided
+    ) {
         super("Function '" + functionName + "' expects between " +
                 expectedMin + " and " + expectedMax + " arguments, but got " + provided);
     }
--- api/src/main/java/com/instancify/scriptify/api/exception/ScriptFunctionArgumentTypeMismatchException.java
+++ api/src/main/java/com/instancify/scriptify/api/exception/ScriptFunctionArgumentTypeMismatchException.java
@@ -1,11 +1,25 @@
 package com.instancify.scriptify.api.exception;
 
+/**
+ * Exception thrown when a function is called with arguments
+ * of incompatible types.
+ */
 public class ScriptFunctionArgumentTypeMismatchException extends ScriptFunctionException {
 
-    public ScriptFunctionArgumentTypeMismatchException(String functionName,
-                                                       int index,
-                                                       Class<?> expected,
-                                                       Class<?> actual) {
+    /**
+     * Creates a new exception for an argument type mismatch.
+     *
+     * @param functionName The name of the function
+     * @param index The index of the argument that failed validation
+     * @param expected The expected type of the argument
+     * @param actual The actual type of the provided argument
+     */
+    public ScriptFunctionArgumentTypeMismatchException(
+            String functionName,
+            int index,
+            Class<?> expected,
+            Class<?> actual
+    ) {
         super("Function '" + functionName + "' argument #" + index +
                 " expected type " + expected.getName() +
                 " but got " + (actual == null ? "null" : actual.getName()));
